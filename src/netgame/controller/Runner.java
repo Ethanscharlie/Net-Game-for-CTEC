383ebbd393536bab30f38c6fe51af40e27e9ac42
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-02 23:01:27 -0600

    Ran autoformatter on Runner

--- src/netgame/controller/Runner.java
+++ src/netgame/controller/Runner.java
@@ -2,11 +2,8 @@ package netgame.controller;
 
 public class Runner
 {
-
-	public static void main(String[] args)
-	{
-		@SuppressWarnings("unused")
-		Controller app = new Controller();
-	}
-
+    public static void main(String[] args)
+    {
+        @SuppressWarnings("unused") Controller app = new Controller();
+    }
 }
